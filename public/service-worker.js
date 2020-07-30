95a870719ab2becf938034773048521a9ace00c1
Esther Min <estherjmin@gmail.com>
2020-07-30 18:08:51 -0400

    edited order of code in server.js

--- public/service-worker.js
+++ public/service-worker.js
@@ -2,7 +2,8 @@ const FILES_TO_CACHE = [
     "/",
     "/index.html",
     "/styles.css",
-    "/dist/bundle.js",
+    "/index.js",
+    "/manifest.webmanifest",
     "/icons/icon-192x192.png",
     "/icons/icon-512x512.png"
 ];
